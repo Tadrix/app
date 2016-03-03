7a4bd279f24e6b75eba2075f5971f2c4a64e2fb5
Tadrix <madlord@gmail.com>
2016-03-03 16:41:10 +0200

    Page models

---
+++ public/js/collections/pages.js
@@ -0,0 +1,8 @@
+define(['underscore', 'backbone', 'models/page'],
+function (_, Backbone, PageModel) {
+  var PagesCollection = Backbone.Collection.extend({
+    model: PageModel,
+    url: '/pages'
+  });
+  return PagesCollection;
+});
